222e4be859b1e8c1335b8242eae3cd5a3af2dde7
Anna Erika <helloannaerika@gmail.com>
2022-09-30 23:27:04 +0200

    set up file upload, WIP pages, and components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Homepage from "./pages/HomePage";
+import AddMovie from './components/AddMovie';
 import RoomListPage from "./pages/RoomListPage";
 import RoomDetailsPage from "./pages/RoomDetailsPage";
 import RoomEditPage from "./pages/RoomEditPage";
@@ -10,27 +11,26 @@ import ItemDetailsPage from "./pages/ItemDetailsPage";
 import ItemEditPage from "./pages/ItemEditPage";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
-import isPrivate from "./components/IsPrivate";
+import IsPrivate from "./components/IsPrivate";
 import IsAnon from "./components/IsAnon";
 
 function App() {
   return (
     <div className="App">
-     
-    <h1>Hi from the Landing Page</h1>
 
     <Navbar />
 
     <Routes>
       <Route exact path="/" element={ <Homepage /> } />
+      <Route path="/movies/add" element={ <AddMovie /> } />
       <Route exact path="/signup" element={ <IsAnon><SignupPage /></IsAnon> } />
       <Route exact path="/login" element={ <IsAnon><LoginPage /></IsAnon> } />
       <Route exact path="/rooms" element={ <RoomListPage /> } />
       <Route exact path="/rooms/:roomId" element={ <RoomDetailsPage /> } />
-      <Route exact path="/rooms/edit/:roomId" element={ <isPrivate><RoomEditPage /></isPrivate>  } />
+      <Route exact path="/rooms/edit/:roomId" element={ <IsPrivate><RoomEditPage /></IsPrivate>  } />
       <Route exact path="/items" element={ <ItemListPage /> } />
       <Route exact path="/items/:itemId" element={ <ItemDetailsPage /> } />
-      <Route exact path="/items/edit/:itemId" element={ <isPrivate><ItemEditPage /></isPrivate>  } />
+      <Route exact path="/items/edit/:itemId" element={ <IsPrivate><ItemEditPage /></IsPrivate>  } />
     </Routes>
 
     </div>
